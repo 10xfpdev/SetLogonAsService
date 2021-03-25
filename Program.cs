f073e413cb7f0e2fb957c0286b3025944e2f8534
fernando <fpgithub@gmail.com>
2021-03-25 13:25:51 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SetLogonAsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String userName = args[0];
+            try
+            {
+                LsaWrapper lsaUtility = new LsaWrapper();
+            
+                lsaUtility.SetRight(userName, "SeServiceLogonRight");
+            
+                Console.WriteLine("Logon as a Service right is granted successfully to " + userName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
